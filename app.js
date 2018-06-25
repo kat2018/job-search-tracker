15aee0c161ce42b1672661efb549c67c4814c75b
kat2018 <kat.vge@gmail.com>
2018-06-25 18:56:53 -0400

    created React app

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 // Mongoose Setup
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
+//Mongoose error handler
+const db = mongoose.connection;
+db.on("connected", () => {
+  console.log("Mongoose Connected Successfully");
+});
+db.on('error', console.error.bind(console, 'connection error:'));
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
